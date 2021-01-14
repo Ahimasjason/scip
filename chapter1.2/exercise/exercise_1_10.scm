0d7d821ff5fc5da7ecaa19b4464c7baa9bf6d242
Ahimasjason <ahimas.jasan@gmail.com>
2021-01-14 08:07:45 +0530

    finishing 1.10

--- chapter1.2/exercise/exercise_1_10.scm
+++ chapter1.2/exercise/exercise_1_10.scm
@@ -9,3 +9,26 @@
 	((= y 1) 2)
 	(else (A (- x 1)
 		 (A x (- y 1))))))
+
+
+
+(A 1 10) 
+
+(A 2 4)
+
+(A 3 3)
+
+;; here x is o so it will return 2*n
+(define (f n) (A 0 n)) 
+
+;;here x is 1 it will return n times 2 -> 2^n
+(define (g n) (A 1 n))
+
+;;; really long one 
+(define (h n) (A 2 n))
+
+(define (k n) (* 5 n n))
+
+(k 2)
+
+(f 2)
